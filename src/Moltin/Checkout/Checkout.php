c735060e7528edb0c8862d660806a985e306b515
Chris Harvey <chris@chrisnharvey.com>
2013-05-06 21:23:41 +0100

    Adding further code to Checkout class

--- src/Moltin/Checkout/Checkout.php
+++ src/Moltin/Checkout/Checkout.php
@@ -3,11 +3,25 @@
 namespace Moltin\Checkout;
 
 use Moltin\Cart\Cart;
+use Omnipay\Common\GatewayFactory;
 
 class Checkout
 {
 	public function __construct(Cart $cart)
 	{
 		$this->cart = $cart;
+
+		// Set the default gateway to Dummy
+		$this->gateway = GatewayFactory::create('Dummy');
+	}
+
+	public function setGateway($gateway)
+	{
+		$this->gateway = GatewayFactory::create($gateway);
+	}
+
+	public function processPayment(array $data)
+	{
+		
 	}
 }
