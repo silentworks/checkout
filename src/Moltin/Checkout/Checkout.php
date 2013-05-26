7439bbbd7851c77dd27f402b537a77357620f89f
Chris Harvey <chris@chrisnharvey.com>
2013-05-26 14:43:29 +0000

    Adding copyright disaimer

--- src/Moltin/Checkout/Checkout.php
+++ src/Moltin/Checkout/Checkout.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * This file is part of Moltin Checkout, a PHP package to
+ * convert your Moltin\Cart object into an order and take
+ * payments.
+ *
+ * Copyright (c) 2013 Moltin Ltd.
+ * http://github.com/moltin/checkout
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package moltin/checkout
+ * @author Chris Harvey <chris@molt.in>
+ * @copyright 2013 Moltin Ltd.
+ * @version dev
+ * @link http://github.com/moltin/checkout
+ *
+ */
+
 namespace Moltin\Checkout;
 
 use Moltin\Cart\Cart;
