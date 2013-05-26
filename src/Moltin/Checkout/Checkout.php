f0d4c2b949b821b616b9ddc58494388c5273e75f
Chris Harvey <chris@chrisnharvey.com>
2013-05-26 14:39:19 +0000

    DocBlocks

--- src/Moltin/Checkout/Checkout.php
+++ src/Moltin/Checkout/Checkout.php
@@ -13,11 +13,22 @@ class Checkout
 	protected $gateway = false;
 	protected $card = array();
 
+	/**
+	 * The constructor
+	 * 
+	 * @param Cart $cart The cart you want to create an order from
+	 */
 	public function __construct(Cart $cart)
 	{
 		$this->cart = $cart;
 	}
 
+	/**
+	 * Set card/billing/shipping data
+	 * 
+	 * @param string|array $key A key or array of key-value pairs
+	 * @param mixed $value The value you want to set $key to
+	 */
 	public function setCard($key, $value = null)
 	{
 		if (is_array($key)) {
@@ -33,6 +44,11 @@ class Checkout
 		return $this;
 	}
 
+	/**
+	 * Return the gateway instance
+	 * 
+	 * @return Omnipay\Common\AbstractGateway The gateway instance
+	 */
 	public function gateway()
 	{
 		$this->checkGateway();
@@ -40,46 +56,98 @@ class Checkout
 		return $this->gateway;
 	}
 
+	/**
+	 * Set the gateway which will be used for payment
+	 * 
+	 * @param string $gateway The name of the gateway
+	 */
 	public function setGateway($gateway)
 	{
 		$this->gateway = GatewayFactory::create($gateway);
 	}
 
+	/**
+	 * Submit an authorize request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function authorize(array $data = array())
 	{
 		return $this->gatewayRequest('authorize', $data);
 	}
 
+	/**
+	 * Submit a complete authorize request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function completeAuthorize(array $data = array())
 	{
 		return $this->gatewayRequest('completeAuthorize', $data);
 	}
 
+	/**
+	 * Submit a capture request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function capture(array $data = array())
 	{
 		$this->gatewayRequest('capture', $data);
 	}
 
+	/**
+	 * Submit a purchase request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function purchase(array $data = array())
 	{
 		return $this->gatewayRequest('purchase', $data);
 	}
 
+	/**
+	 * Submit a complete purchase request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function completePurchase(array $data = array())
 	{
 		return $this->gatewayRequest('completePurchase', $data);
 	}
 
+	/**
+	 * Submit a refund request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function refund(array $data = array())
 	{
 		return $this->gatewayRequest('refund', $data);
 	}
 
+	/**
+	 * Submit a void request to the gateway
+	 * 
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	public function void(array $data = array())
 	{
 		return $this->gatewayRequest('void', $data);
 	}
 
+	/**
+	 * Check if the gateway has been set
+	 * 
+	 * @return void
+	 */
 	protected function checkGateway()
 	{
 		if ( ! $this->gateway) {
@@ -87,6 +155,13 @@ class Checkout
 		}
 	}
 
+	/**
+	 * Submit a request to the gateway
+	 * 
+	 * @param  string $method The method to submit
+	 * @param  array  $data The card/billing/shipping data
+	 * @return Omnipay\Common\RequestInterface
+	 */
 	protected function gatewayRequest($method, array $data = array())
 	{
 		$this->checkGateway();
@@ -106,6 +181,12 @@ class Checkout
 		}
 	}
 
+	/**
+	 * Set a card property
+	 * 
+	 * @param string $property
+	 * @param mixed $value
+	 */
 	public function __set($property, $value)
 	{
 		$this->card[$property] = $value;
