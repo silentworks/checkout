377f201209c039898411a04f76f3f45efe2b3e23
Chris Harvey <chris@chrisnharvey.com>
2013-05-27 11:43:59 +0000

    Removing braces from interface

--- src/Moltin/Checkout/Storage/TransactionInterface.php
+++ src/Moltin/Checkout/Storage/TransactionInterface.php
@@ -24,8 +24,5 @@ namespace Moltin\Checkout\Storage;
 interface TransactionInterface
 {
     // Return order transaction data as an array....
-    public function getTransaction($transId)
-    {
-
-    }
+    public function getTransaction($transId);
 }
