a7cef6874946079828567f0cedae4029fbfaa545
Chris Harvey <chris@chrisnharvey.com>
2013-05-27 11:32:38 +0000

    Removing braces, this is an interface

--- src/Moltin/Checkout/Storage/OrderInterface.php
+++ src/Moltin/Checkout/Storage/OrderInterface.php
@@ -24,39 +24,21 @@ namespace Moltin\Checkout\Storage;
 interface OrderInterface
 {
     // Return order id
-    public function createOrder(array $order)
-    {
-
-    }
+    public function createOrder(array $order);
 
     // Return true or false
-    public function updateOrder($id, array $order)
-    {
-
-    }
+    public function updateOrder($id, array $order);
 
     // Return true or false
     // This should also delete items
-    public function deleteOrder($id)
-    {
-
-    }
+    public function deleteOrder($id);
 
     // Return the item ID
-    public function insertItem($id, $item)
-    {
-
-    }
+    public function insertItem($id, $item);
 
     // Return true or false
-    public function updateItem($itemId, $item)
-    {
-
-    }
+    public function updateItem($itemId, $item);
 
     // Return true or false
-    public function deleteItem($itemId)
-    {
-
-    }
+    public function deleteItem($itemId);
 }
